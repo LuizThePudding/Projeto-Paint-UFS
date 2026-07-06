190b3f4e487ed567500cec52e8b6f7a1364b1ab3
Andrey Ariel Silva de Almeida <andreyarielsilvadealmeida@gmail.com>
2026-07-06 15:55:23 -0300

    Update janelaPaint.py
    atualizaçao tirando as coisas que fazem o canvas e option menus, porque estavam todos juntos

--- View/janelaPaint.py
+++ View/janelaPaint.py
@@ -1,49 +1,11 @@
 from tkinter import *
 from tkinter import ttk
+from Model import *
+
 
 class JanelaPaint():
-    def __init__(self):
-        self.root = Tk()  #isso daqui pelo visto tem que ficar ou no main ou no controller
-        self.frame = Frame(self.root) #esse tambem
-
-        # Widgets arranjados com Layout grid dentro de frame
-        paddings = {'padx': 5, 'pady': 5} 
-
-        # label
-        self.label = ttk.Label(self.frame, text='Paint.v0,4')
-        self.label.grid(column=0, row=0, sticky=W, **paddings)
-
-        self.label_cor = ttk.Label(self.frame, text='Borda:')
-        self.label_cor.grid(column=2, row=0, sticky=W, **paddings)
-
-        self.label_preenchimento = ttk.Label(self.frame, text='Preenchimento:')
-        self.label_preenchimento.grid(column=4, row=0, sticky=W, **paddings)
-
-        # option menu das figuras
-        self.tipo_figura_var = StringVar(self.root) # Guarda o tipo de figura selecionado no option menu
-        self.option_menu = ttk.OptionMenu(self.frame, self.tipo_figura_var,
-                                     'Linha', 'Linha', 'Rabisco', 'Circulo', 'Oval', 'Retangulo')
-        self.option_menu.grid(column=1, row=0, sticky=W, **paddings)
-
-        # option menu de cor de borda
-        self.tipo_bord_var = StringVar(self.root)
-        self.option_menu_bord = ttk.OptionMenu(self.frame, self.tipo_bord_var,
-                                          'Preto', 'Preto', 'Azul', 'Verde', 'Vermelho')
-        self.option_menu_bord.grid(column=3, row=0, sticky=W, **paddings)
-
-        #option menu cor de preenchiemnto
-        self.tipo_preenchimento_var = StringVar(self.root)
-        option_menu_preenchimento = ttk.OptionMenu(self.frame, self.tipo_preenchimento_var,
-                                                     'Transparente', 'Transparente', 'Preto', 'Azul', 'Verde', 'Vermelho')
-        option_menu_preenchimento.grid(column=5, row=0, sticky=W, **paddings)
-
-        # Área de desenho
-        self.canvas = Canvas(self.frame, bg='white', width=900, height=600)
-        self.canvas.grid(column=0, row=1, columnspan=6, sticky=W, **paddings)
-
-        self.frame.pack()
-
-
+    def __init__(self, canvas):
+       self.canvas = canvas
     def desenhar(self, figura):
         tipo = figura.tipo
         coords = figura.values
